e8425bea4a70bf8fd4a6a218300aaeca76dec8d2
monica-arbelaez <moniarbe233@gmail.com>
2021-05-22 12:14:25 -0500

    Subiendo ejercicio3 taller Java

---
+++ src/main/java/ejercicio3/Ejercicio3.java
@@ -0,0 +1,22 @@
+package ejercicio3;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    private double radio;
+
+    public void areaCirculo(double radio) {
+        System.out.println("El area del circulo es: " + Math.PI * Math.pow(radio, 2));
+    }
+
+
+    public static void main(String[] args) {
+        Scanner entradaScanner = new Scanner(System.in);
+        Ejercicio3 miEjercicio3 = new Ejercicio3();
+
+        System.out.println("ingresa el radio del circulo");
+        double radio = Integer.parseInt(entradaScanner.nextLine());
+
+        miEjercicio3.areaCirculo(radio);
+    }
+}
