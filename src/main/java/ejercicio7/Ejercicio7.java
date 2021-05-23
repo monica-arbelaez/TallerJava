e75fdcdc993424be2aac3faa426b3ee38ebd35ac
monica-arbelaez <moniarbe233@gmail.com>
2021-05-23 00:18:12 -0500

    Subiendo ejercicio7 taller Java

---
+++ src/main/java/ejercicio7/Ejercicio7.java
@@ -0,0 +1,33 @@
+package ejercicio7;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+    private int numero;
+
+
+    public  void VerNumero(int numero){
+        Scanner entradaScanner = new Scanner(System.in);
+        if(numero>=0){
+            System.out.println("el número que ingreso es  : "+numero);
+        }else{
+            do {
+                System.out.println("ingrese un numero nuvamente");
+                numero =  Integer.parseInt(entradaScanner.nextLine());
+            }
+            while (numero<=0);
+                System.out.println("el número que ingreso es  : "+numero);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner entradaScanner = new Scanner(System.in);
+        Ejercicio7 miEjercicio7 = new Ejercicio7();
+
+        System.out.println("ingresa un número" );
+        int numero = Integer.parseInt(entradaScanner.nextLine());
+
+        miEjercicio7.VerNumero(numero);
+    }
+}
